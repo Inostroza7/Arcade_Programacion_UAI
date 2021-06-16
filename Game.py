70aae9b9d58ace7f36afdb70abc73873341839b7
Inostroza7 <gusinostrozar@gmail.com>
2021-06-15 21:56:39 -0400

    Upload

--- Game.py
+++ Game.py
@@ -27,9 +27,114 @@ class TurningSprite(arcade.Sprite):
         super().update()
         self.angle = math.degrees(math.atan2(self.change_y, self.change_x))
 
+class ShipSprite(arcade.Sprite):
+    """
+    Sprite que representa nuestra nave
+
+    Herencia de arcade.Sprite.
+    """
+    def __init__(self, filename, scale):
+        """ Definimos nuestra nave. """
+
+        # Super llama a la funcion padre
+        super().__init__(filename, scale)
+
+        # El ángulo se hereda de la funcion padre
+        self.thrust = 0
+        self.speed = 0
+        self.max_speed = 4
+        self.drag = 0.05
+        self.respawning = 0
+
+        # Marca para reaparecer o respawn.
+        self.respawn()
+
+    def respawn(self):
+        """
+        Llamamos a esta funcion cuando se destruye la nave y se necesita reaparecer.
+        respawn o reaparecer es un tiempo de invulnerabilidad.
+        """
+        # Si estamos en medio de un Respawning este es 0.
+        self.respawning = 1
+        self.center_x = SCREEN_WIDTH / 2
+        self.center_y = SCREEN_HEIGHT / 2
+        self.angle = 0
+
+    def update(self): # TODO verificar si la nave estará fija en el centro o se movera
+        """
+        Actualiza nuestra posición y otros partículares.
+        """
+        if self.respawning:
+            self.respawning += 1
+            self.alpha = self.respawning / 500.0
+            if self.respawning > 250:
+                self.respawning = 0
+                self.alpha = 1
+        if self.speed > 0:
+            self.speed -= self.drag
+            if self.speed < 0:
+                self.speed = 0
+
+        if self.speed < 0:
+            self.speed += self.drag
+            if self.speed > 0:
+                self.speed = 0
+
+        self.speed += self.thrust
+        if self.speed > self.max_speed:
+            self.speed = self.max_speed
+        if self.speed < -self.max_speed:
+            self.speed = -self.max_speed
+
+        self.change_x = -math.sin(math.radians(self.angle)) * self.speed
+        self.change_y = math.cos(math.radians(self.angle)) * self.speed
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        """ Llama a la clase padre. """
+        super().update()
+
+
+class AsteroidSprite(arcade.Sprite): # TODO Modificar esto por naves enemigas
+    """ Sprite that represents an asteroid. """
+
+    def __init__(self, image_file_name, scale):
+        super().__init__(image_file_name, scale=scale)
+        self.size = 0
+
+    def update(self):
+        """ Move the asteroid around. """
+        super().update()
+        if self.center_x < LEFT_LIMIT:
+            self.center_x = RIGHT_LIMIT
+        if self.center_x > RIGHT_LIMIT:
+            self.center_x = LEFT_LIMIT
+        if self.center_y > TOP_LIMIT:
+            self.center_y = BOTTOM_LIMIT
+        if self.center_y < BOTTOM_LIMIT:
+            self.center_y = TOP_LIMIT
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#############################
 arcade.open_window(ancho,alto,"SPACE JAM", True,True)
 
 colorLetras = arcade.color.SKY_BLUE
