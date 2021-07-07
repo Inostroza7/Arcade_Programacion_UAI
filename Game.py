2ae46ad07087379c686eb4f31ade1c7998067efd
Inostroza7 <gusinostrozar@gmail.com>
2021-07-07 10:14:34 -0400

    Upload

--- Game.py
+++ Game.py
@@ -5,9 +5,11 @@ import os
 
 
 # Dimensiones pantalla
-SCREEN_WIDTH = 1300
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Space Jam"
+
+# Margen entre el borde de la pantalla
 OFFSCREEN_SPACE = 300
 LEFT_LIMIT = -OFFSCREEN_SPACE
 RIGHT_LIMIT = SCREEN_WIDTH + OFFSCREEN_SPACE
@@ -20,6 +22,69 @@ SCALE_ENEMY = 1 # Enemigo
 SCALE_LIFE = 1 # Vida
 SCALE_TIME = 1 # Tiempo
 SCALE_SCORE = 1  # Puntaje
+
+# Características de la Física del Juego
+MOVEMENT_SPEED = 5
+ENEMY_SPEED = 2
+EMEMY2_SPEED = 2.5
+
+# Musica
+MUSIC_VOLUME = 0.03
+MUSIC_VOLUMEHIGH = 0.25
+
+class MenuView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+        self.texture = arcade.load_texture("introWallpaper.png")
+        # Restablecer la ventana gráfica, necesaria si tenemos un juego de desplazamiento y necesitamos
+        # para restablecer la ventana gráfica al inicio para que podamos ver lo que dibujamos.
+        arcade.set_viewport(0, SCREE_WIDHT - 1, 0, SCREE_HEIGHT - 1)
+
+    def on_draw(self):
+        """ Dibuja esta vista """
+        arcade.start_render()
+        self.texture.draw_sized(SCREE_WIDHT / 2, SCREE_HEIGHT / 2,
+                                SCREE_WIDHT, SCREE_HEIGHT)
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ Si el usuario presiona el botón del mouse, inicia el juego. """
+        game_view = MyGame()
+        game_view.setup()
+        self.window.show_view(game_view)
+
+class Enemy(arcade.Sprite):
+
+    def follow_sprite(self, player_sprite):
+        # Esta función es para que el Enemy (self) se mueva hacia el personaje (player.sprite).
+
+        if self.center_y < player_sprite.center_y:
+            self.center_y += min(ENEMY_SPEED, player_sprite.center_y - self.center_y)
+        elif self.center_y > player_sprite.center_y:
+            self.center_y -= min(ENEMY_SPEED, self.center_y - player_sprite.center_y)
+
+        if self.center_x < player_sprite.center_x:
+            self.center_x += min(ENEMY_SPEED, player_sprite.center_x - self.center_x)
+        elif self.center_x > player_sprite.center_x:
+            self.center_x -= min(ENEMY_SPEED_SPEED, self.center_x - player_sprite.center_x)
+
+class Enemy2(arcade.Sprite):
+
+    def follow_sprite(self, player_sprite):
+        # Esta función es para que el Enemy2 (self) se mueva hacia el personaje (player.sprite).
+
+        if self.center_y < player_sprite.center_y:
+            self.center_y += min(ENEMY2_SPEED, player_sprite.center_y - self.center_y)
+        elif self.center_y > player_sprite.center_y:
+            self.center_y -= min(ENEMY2_SPEED, self.center_y - player_sprite.center_y)
+
+        if self.center_x < player_sprite.center_x:
+            self.center_x += min(ENEMYw_SPEED, player_sprite.center_x - self.center_x)
+        elif self.center_x > player_sprite.center_x:
+            self.center_x -= min(ENEMY2_SPEED_SPEED, self.center_x - player_sprite.center_x)
+
+
+
 
 class TurningSprite(arcade.Sprite):
     """ Sprite que establece su ángulo en la dirección en la que viaja la nave. """
